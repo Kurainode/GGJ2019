99bf37683abb7ab9843e7b584a0ca93e50ee9ef6
Kurainode <kurainode@gmail.com>
2019-01-26 11:28:23 +0100

    Added basic player movements

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * speed;
+    }
+}
