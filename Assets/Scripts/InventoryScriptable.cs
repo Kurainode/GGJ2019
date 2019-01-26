d1e0fc3da59941a87381f82b77ad081221ed7fcc
Rémy Tauziac <tauziac0remy@gmail.com>
2019-01-27 00:31:10 +0100

    Trying to solving pull fast forward automerge conflicts

---
+++ Assets/Scripts/InventoryScriptable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryScriptable : MonoBehaviour
+{
+
+    const string GOName = "Inventory";
+
+    public Image inventory;
+    public AudioClip openSound;
+    public AudioClip closeSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.Find(GOName) != null)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Menu"))
+        {
+            if (inventory.gameObject.activeInHierarchy == false)
+            {
+                GetComponent<GlobalMessageEventSender>().GlobalMessage("OpenInventory");
+            }
+            else
+            {
+                GetComponent<GlobalMessageEventSender>().GlobalMessage("CloseInventory");
+            }
+        }
+    }
+
+    public void OpenInventory()
+    {
+        //inventory.gameObject.SetActive(true);
+        // TODO: setup layout and default page
+        GetComponent<AudioSource>().PlayOneShot(openSound);
+        GetComponent<Animator>().SetTrigger("toggleVisibility");
+    }
+
+    public void CloseInventory()
+    {
+        //inventory.gameObject.SetActive(false);
+        GetComponent<AudioSource>().PlayOneShot(closeSound);
+        GetComponent<Animator>().SetTrigger("toggleVisibility");
+    }
+}
